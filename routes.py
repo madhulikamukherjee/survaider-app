bde2a39732d5b2bc17327bb313acb1e69e283bc5
Prashant Sinha <prashnts@live.com>
2015-02-27 08:22:05 +0530

    Fixes

--- routes.py
+++ routes.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-# coding=utf-8
+# -*- coding: utf-8 -*-
 #.--. .-. ... .... -. - ... .-.-.- .. -.
 
-from flask import Flask
+from flask import Flask, render_template
 import config
 import survaider
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html', name="LOL")
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
