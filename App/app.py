b1691aa3912b97b89975306df1ce27c76066cd15
Prashant Sinha <prashnts@live.com>
2015-04-06 00:31:50 +0530

    removed unused things

--- App/app.py
+++ App/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from user import usr
+from game import game
 from config import config
 
 app = Flask(__name__)
 app.register_blueprint(usr, url_prefix = '/usr')
+app.register_blueprint(game, url_prefix = '/game')
 
 if __name__ == "__main__":
     app.run(host = config['HOST'], port = config['PORT'], debug = config['DEBUG'])
