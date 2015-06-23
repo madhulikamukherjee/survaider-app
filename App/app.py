b5a46a2fd3d39a1a66fae4a3a7de580e8960430e
Prashant Sinha <prashnts@live.com>
2015-06-23 23:48:20 +0530

    More templates

--- App/app.py
+++ App/app.py
@@ -3,13 +3,15 @@ from user import usr
 from game import game
 from survey import survey
 from web import web
-from config import config
+from config import config, static_route_prefix
 
 app = Flask(__name__)
 app.register_blueprint(usr, url_prefix = '/usr')
 app.register_blueprint(game, url_prefix = '/game')
 app.register_blueprint(survey, url_prefix = '/survey')
 app.register_blueprint(web, url_prefix = '/web')
+
+app.config['SRP'] = static_route_prefix
 
 if __name__ == "__main__":
     app.run(host = config['HOST'], port = config['PORT'], debug = config['DEBUG'])
--- App/web/__init__.py
+++ App/web/__init__.py
@@ -4,9 +4,11 @@
 
 from flask import Flask, Blueprint, render_template, request, jsonify
 
+from config import static_route_prefix
+
 web = Blueprint('web', __name__, template_folder='templates')
 
 @web.route('/')
 def get():
-    return render_template("master_full.html")
+    return render_template("master_full.html", master = "test")
 
