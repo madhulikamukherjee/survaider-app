5f0f2b30663c609d3c37b8b8b97eeac46e9b8bc0
Prashant Sinha <prashant@ducic.ac.in>
2015-10-13 16:27:21 +0530

    Proper Manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,16 @@
 # -*- coding: utf-8 -*-
 #.--. .-. ... .... -. - ... .-.-.- .. -.
 
+from flask.ext.script import Manager
 from survaider import create_app, app
 
-create_app()
-app.run()
+manager = Manager(app)
+
+@manager.command
+def run():
+    "Runs the App"
+    create_app()
+    app.run()
+
+if __name__ == "__main__":
+    manager.run()
