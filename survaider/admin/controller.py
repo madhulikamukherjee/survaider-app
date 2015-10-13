271a5a213376e54a51ba8d751183a40eaac93ee0
Prashant Sinha <prashant@ducic.ac.in>
2015-10-13 20:50:46 +0530

    Refactors

--- survaider/admin/controller.py
+++ survaider/admin/controller.py
@@ -7,16 +7,23 @@ from flask_admin.contrib.mongoengine import ModelView
 from flask_admin import helpers as admin_helpers
 
 from survaider.user.model import Role, User
+from survaider.survey.model import Survey, Response
 from survaider.admin.model import BaseView
 
 from survaider import app
 
-admin = Admin(app, name='BioDB Admin', template_mode='bootstrap3', base_template='my_master.html')
+admin = Admin(app, name='Survaider', base_template='my_master.html')
 
 class UserView(BaseView):
     can_create = False
-    column_searchable_list = ('username', 'email')
+    can_view_details = True
+    column_searchable_list = ['email']
     column_exclude_list = ['password', 'added']
+
+class SurveyView(BaseView):
+    can_view_details = True
 
 admin.add_view(UserView(User))
 admin.add_view(BaseView(Role))
+admin.add_view(SurveyView(Survey))
+admin.add_view(BaseView(Response))
--- survaider/survey/model.py
+++ survaider/survey/model.py
@@ -2,19 +2,25 @@
 # -*- coding: utf-8 -*-
 #.--. .-. ... .... -. - ... .-.-.- .. -.
 
+import datetime
+import uuid
+
 from survaider import db
 
 class Survey(db.Document):
-    s_id        = db.StringField()
-    added       = db.
+    s_id        = db.StringField(unique = True, default = str(uuid.uuid4()))
+    added       = db.DateTimeField(default = datetime.datetime.now)
 
-    structure   =
+    structure   = db.DictField()
 
     name        = db.StringField(max_length = 80)
     description = db.StringField(max_length = 255)
 
-class Response(db.Document):
-    r_id        =
-    s_id        =
+    def __unicode__(self):
+        return self.s_id
 
-    responses   =
+class Response(db.Document):
+    r_id        = db.StringField(unique = True, default = str(uuid.uuid4()))
+    s_id        = db.ReferenceField(Survey)
+
+    responses   = db.DictField()
