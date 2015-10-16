502713cdf0c8c72f6ccf40c10bba56130ab03879
Prashant Sinha <prashant@ducic.ac.in>
2015-10-16 23:21:11 +0530

    Survey Controller

--- survaider/survey/controller.py
+++ survaider/survey/controller.py
@@ -9,3 +9,30 @@ REST API End Points
 from flask_restful import Resource
 
 from survey.model import Survey, Response
+
+class Survey(Resource):
+    def get(self):
+        return {}
+
+    def put(self):
+        return
+
+    def post(self):
+        return
+
+    def delete(self):
+        return
+
+class Response(Resource):
+    def get(self):
+        return {}
+
+    def put(self):
+        return
+
+    def post(self):
+        return
+
+    def delete(self):
+        return
+
