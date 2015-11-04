10f7c2a096c972d95282ac07536ff6ae8dbee456
Prashant Sinha <prashant@ducic.ac.in>
2015-11-04 22:50:18 +0530

    Part ways into facebook.

---
+++ survaider/minions/signals.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#.--. .-. ... .... -. - ... .-.-.- .. -.
+
+
+class Social(object):
+
+    @staticmethod
+    def add_unknown_user(**kwargs):
+        print("hefjhksdjn re")
+        print(kwargs)
--- survaider/social/controller.py
+++ survaider/social/controller.py
@@ -3,8 +3,35 @@
 #.--. .-. ... .... -. - ... .-.-.- .. -.
 
 from flask.ext.social import Social
+from flask.ext.social.datastore import MongoEngineConnectionDatastore
+from flask.ext.social import connection_created, connection_failed, connection_removed, login_failed, login_completed
 
-from survaider import app
+from survaider import db, app
 from survaider.social.model import Connection
+from survaider.minions.signals import Social as Signal_Social
 
-social = Social(app, Connection)
+social = Social(app, MongoEngineConnectionDatastore(db, Connection))
+
+@connection_created.connect_via(app)
+def on_connection_created(sender, **kwargs):
+    app.logger.debug('Connection Created')
+
+@connection_failed.connect_via(app)
+def on_login_failed1(sender, provider, oauth_response):
+    app.logger.debug('Social Login Failed via %s; '
+                     '&oauth_response=%s' % (provider.name, oauth_response))
+
+@connection_removed.connect_via(app)
+def on_login_failed4(sender, provider, oauth_response):
+    app.logger.debug('Social Login Failed via %s; '
+                     '&oauth_response=%s' % (provider.name, oauth_response))
+
+@login_failed.connect_via(app)
+def on_login_failed3(sender, provider, oauth_response):
+    app.logger.debug('Social Login Failed via %s; '
+                     '&oauth_response=%s' % (provider.name, oauth_response))
+
+@login_completed.connect_via(app)
+def on_login_failed33(sender, provider, oauth_response):
+    app.logger.debug('Social Login Failed via %s; '
+                     '&oauth_response=%s' % (provider.name, oauth_response))
