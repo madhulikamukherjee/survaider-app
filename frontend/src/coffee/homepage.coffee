c7fed19c5c69651312eec245adab103c12b611e2
Prashant Sinha <prashant@ducic.ac.in>
2015-11-17 19:08:11 +0530

    Final fixes

--- frontend/src/coffee/homepage.coffee
+++ frontend/src/coffee/homepage.coffee
@@ -3,5 +3,18 @@ $(document).ready ->
   $('#invite').on 'click', (el) ->
     $("#invite").parent("li").parent("ul").eq(0).toggleClass 'open'
 
-  $('#invite_form').on 'submit', ->
-    $
+  $('#invite_form').on 'submit', (e, el) ->
+    e.preventDefault()
+    submit = $('#invite_form button[type=submit]')
+    submit.attr('disabled', 'true')
+    submit.html('Processing')
+    $.ajax
+      url: 'http://backstage.ducic.ac.in/passthru/madhulika@survaider.com'
+      data: $('#invite_form').serialize()
+      type: 'GET'
+    .done ->
+      $('#invite').html("Request sent!")
+      $('#invite').click()
+      $('#invite').off 'click'
+    .fail ->
+      alert('Please try Again')
--- survaider/static/domain/homepage.js
+++ survaider/static/domain/homepage.js
@@ -3,8 +3,23 @@
     $('#invite').on('click', function(el) {
       return $("#invite").parent("li").parent("ul").eq(0).toggleClass('open');
     });
-    return $('#invite_form').on('submit', function() {
-      return $;
+    return $('#invite_form').on('submit', function(e, el) {
+      var submit;
+      e.preventDefault();
+      submit = $('#invite_form button[type=submit]');
+      submit.attr('disabled', 'true');
+      submit.html('Processing');
+      return $.ajax({
+        url: 'http://backstage.ducic.ac.in/passthru/madhulika@survaider.com',
+        data: $('#invite_form').serialize(),
+        type: 'GET'
+      }).done(function() {
+        $('#invite').html("Request sent!");
+        $('#invite').click();
+        return $('#invite').off('click');
+      }).fail(function() {
+        return alert('Please try Again');
+      });
     });
   });
 
