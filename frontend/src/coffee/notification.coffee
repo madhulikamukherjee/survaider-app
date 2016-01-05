90cd52176a3f2eb0581270467188b6c69e6d3920
Prashant Sinha <prashant@ducic.ac.in>
2016-01-05 17:36:52 +0530

    Migrated the notifications to Backbone Library.

--- frontend/src/coffee/notification.coffee
+++ frontend/src/coffee/notification.coffee
@@ -1,19 +1,67 @@
+
+class SurveyTicketNotification extends Backbone.Model
+  defaults:
+    type: 'SurveyTicket'
+
+class SurveyResponseNotification extends Backbone.Model
+  defaults:
+    type: 'SurveyResponseNotification'
+
+  initialize: ->
+    @template = Survaider.Templates['notification.survey.response.tile']
+
+class NotificationCollection extends Backbone.Collection
+  model: (attr, options) ->
+    switch attr.type
+      when 'SurveyTicket'
+        return new SurveyTicketNotification attr, options
+      when 'SurveyResponseNotification'
+        return new SurveyResponseNotification attr, options
+
+class NotificationView extends Backbone.View
+
+  initialize: (options) ->
+    {@parentView} = options
+
+  render: ->
+    @$el.html @model.template({dat: @model.attributes})
+    return @
+
+class NotificationDock extends Backbone.View
+  initialize: (options) ->
+    {selector, @notif, @bootstrapData} = options
+    @collection = new NotificationCollection
+    @collection.reset(@bootstrapData)
+    @setElement $ selector
+    @render()
+    @addAll()
+
+  render: ->
+    @$el.html('')
+
+  addOne: (fieldDat, _, options) ->
+    view = new NotificationView
+      model: fieldDat
+      parentView: @
+    @$el.append view.render().el
+
+  addAll: ->
+    @collection.each @addOne, @
+
+class Notification
+  constructor: (opts={}) ->
+    _.extend @, Backbone.Events
+    args = _.extend opts, {notif: @}
+    @mainView = new NotificationDock args
+
+window.Notification = Notification
+
+if module?
+  module.exports = Notification
+else
+  window.Notification = Notification
+
 NotificationHelper =
-  notification_tiles:
-    init: ->
-      @container = $('#card_dock')
-
-    append: (dat) ->
-      template = Survaider.Templates['notification.survey.response.tile']
-      attrs =
-        expand: if true then 'expanded' else ''
-        narrow: if true then '' else 'narrow'
-
-      el = $ template dat: dat, attrs: attrs
-      @container.append(el)
-
-      Waves.attach el.find '.parent-unit'
-
   nav_menu: ->
     if $('.cd-stretchy-nav').length > 0
       stretchyNavs = $('.cd-stretchy-nav')
@@ -25,15 +73,20 @@ NotificationHelper =
           stretchyNav.toggleClass 'nav-is-visible'
 
       $(document).on 'click', (event) ->
-        !$(event.target).is('.cd-nav-trigger') and !$(event.target).is('.cd-nav-trigger span') and stretchyNavs.removeClass('nav-is-visible')
+        if !$(event.target).is('.cd-nav-trigger') and
+           !$(event.target).is('.cd-nav-trigger span')
+          stretchyNavs.removeClass('nav-is-visible')
 
 $(document).ready ->
-  NotificationHelper.notification_tiles.init()
-  Waves.init()
+  # NotificationHelper.notification_tiles.init()
+  # Waves.init()
 
   $.getJSON '/api/notifications/surveyresponsenotification', (data) ->
     # $('.spinner').hide()
-    NotificationHelper.notification_tiles.append(dat) for dat in data.data
+    # NotificationHelper.notification_tiles.append(dat) for dat in data.data
+    notif = new Notification
+      selector: '#card_dock'
+      bootstrapData: data.data
 
   NotificationHelper.nav_menu()
 
--- survaider/static/domain/notification.js
+++ survaider/static/domain/notification.js
@@ -1,26 +1,148 @@
 (function() {
-  var NotificationHelper;
+  var Notification, NotificationCollection, NotificationDock, NotificationHelper, NotificationView, SurveyResponseNotification, SurveyTicketNotification,
+    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
+
+  SurveyTicketNotification = (function(superClass) {
+    extend(SurveyTicketNotification, superClass);
+
+    function SurveyTicketNotification() {
+      return SurveyTicketNotification.__super__.constructor.apply(this, arguments);
+    }
+
+    SurveyTicketNotification.prototype.defaults = {
+      type: 'SurveyTicket'
+    };
+
+    return SurveyTicketNotification;
+
+  })(Backbone.Model);
+
+  SurveyResponseNotification = (function(superClass) {
+    extend(SurveyResponseNotification, superClass);
+
+    function SurveyResponseNotification() {
+      return SurveyResponseNotification.__super__.constructor.apply(this, arguments);
+    }
+
+    SurveyResponseNotification.prototype.defaults = {
+      type: 'SurveyResponseNotification'
+    };
+
+    SurveyResponseNotification.prototype.initialize = function() {
+      return this.template = Survaider.Templates['notification.survey.response.tile'];
+    };
+
+    return SurveyResponseNotification;
+
+  })(Backbone.Model);
+
+  NotificationCollection = (function(superClass) {
+    extend(NotificationCollection, superClass);
+
+    function NotificationCollection() {
+      return NotificationCollection.__super__.constructor.apply(this, arguments);
+    }
+
+    NotificationCollection.prototype.model = function(attr, options) {
+      switch (attr.type) {
+        case 'SurveyTicket':
+          return new SurveyTicketNotification(attr, options);
+        case 'SurveyResponseNotification':
+          return new SurveyResponseNotification(attr, options);
+      }
+    };
+
+    return NotificationCollection;
+
+  })(Backbone.Collection);
+
+  NotificationView = (function(superClass) {
+    extend(NotificationView, superClass);
+
+    function NotificationView() {
+      return NotificationView.__super__.constructor.apply(this, arguments);
+    }
+
+    NotificationView.prototype.initialize = function(options) {
+      return this.parentView = options.parentView, options;
+    };
+
+    NotificationView.prototype.render = function() {
+      this.$el.html(this.model.template({
+        dat: this.model.attributes
+      }));
+      return this;
+    };
+
+    return NotificationView;
+
+  })(Backbone.View);
+
+  NotificationDock = (function(superClass) {
+    extend(NotificationDock, superClass);
+
+    function NotificationDock() {
+      return NotificationDock.__super__.constructor.apply(this, arguments);
+    }
+
+    NotificationDock.prototype.initialize = function(options) {
+      var selector;
+      selector = options.selector, this.notif = options.notif, this.bootstrapData = options.bootstrapData;
+      this.collection = new NotificationCollection;
+      this.collection.reset(this.bootstrapData);
+      this.setElement($(selector));
+      this.render();
+      return this.addAll();
+    };
+
+    NotificationDock.prototype.render = function() {
+      return this.$el.html('');
+    };
+
+    NotificationDock.prototype.addOne = function(fieldDat, _, options) {
+      var view;
+      view = new NotificationView({
+        model: fieldDat,
+        parentView: this
+      });
+      return this.$el.append(view.render().el);
+    };
+
+    NotificationDock.prototype.addAll = function() {
+      return this.collection.each(this.addOne, this);
+    };
+
+    return NotificationDock;
+
+  })(Backbone.View);
+
+  Notification = (function() {
+    function Notification(opts) {
+      var args;
+      if (opts == null) {
+        opts = {};
+      }
+      _.extend(this, Backbone.Events);
+      args = _.extend(opts, {
+        notif: this
+      });
+      this.mainView = new NotificationDock(args);
+    }
+
+    return Notification;
+
+  })();
+
+  window.Notification = Notification;
+
+  if (typeof module !== "undefined" && module !== null) {
+    module.exports = Notification;
+  } else {
+    window.Notification = Notification;
+  }
 
   NotificationHelper = {
-    notification_tiles: {
-      init: function() {
-        return this.container = $('#card_dock');
-      },
-      append: function(dat) {
-        var attrs, el, template;
-        template = Survaider.Templates['notification.survey.response.tile'];
-        attrs = {
-          expand: true ? 'expanded' : '',
-          narrow: true ? '' : 'narrow'
-        };
-        el = $(template({
-          dat: dat,
-          attrs: attrs
-        }));
-        this.container.append(el);
-        return Waves.attach(el.find('.parent-unit'));
-      }
-    },
     nav_menu: function() {
       var stretchyNavs;
       if ($('.cd-stretchy-nav').length > 0) {
@@ -35,24 +157,21 @@
           });
         });
         return $(document).on('click', function(event) {
-          return !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') && stretchyNavs.removeClass('nav-is-visible');
+          if (!$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span')) {
+            return stretchyNavs.removeClass('nav-is-visible');
+          }
         });
       }
     }
   };
 
   $(document).ready(function() {
-    NotificationHelper.notification_tiles.init();
-    Waves.init();
     $.getJSON('/api/notifications/surveyresponsenotification', function(data) {
-      var dat, i, len, ref, results;
-      ref = data.data;
-      results = [];
-      for (i = 0, len = ref.length; i < len; i++) {
-        dat = ref[i];
-        results.push(NotificationHelper.notification_tiles.append(dat));
-      }
-      return results;
+      var notif;
+      return notif = new Notification({
+        selector: '#card_dock',
+        bootstrapData: data.data
+      });
     });
     return NotificationHelper.nav_menu();
   });
