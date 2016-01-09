c3f28830e41b90b2bccbb54d9a246ffd3cc27adc
Prashant Sinha <prashant@ducic.ac.in>
2016-01-09 14:03:00 +0530

    Finished more UI templates.

--- survaider/static/domain/notification.vendor.js
+++ survaider/static/domain/notification.vendor.js
@@ -23122,37 +23122,27 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<li class="notification survey-response" data-card="parent">\n  <small><span data-livestamp="' +
+__p += '<li class="notification survey-response" data-card="parent">\n  <section class="title">\n    <i class="fa fa-bolt icon"></i>\n    <p>Subscribed Notification</p>\n    <p>\n      <i class="fa fa-clock-o"></i>\n      <span data-livestamp="' +
 ((__t = ( dat.acquired )) == null ? '' : __t) +
-'">(Loading)</span></small>\n  <h1>' +
+'">' +
+((__t = ( dat.acquired )) == null ? '' : __t) +
+'</span>\n    </p>\n    <button><i class="fa fa-times"></i></button>\n  </section>\n\n  <section class="main">\n    <h2>Response at <strong>' +
+((__t = ( dat.survey.name )) == null ? '' : __t) +
+'</strong>.</h2>\n    ';
+ if (dat.root.id !== dat.survey.id) { ;
+__p += '\n    <h3>Main Survey: ' +
 ((__t = ( dat.root.name )) == null ? '' : __t) +
-'</h1>\n  <small>' +
-((__t = ( dat.root.id )) == null ? '' : __t) +
-'</small>\n  <small>' +
-((__t = ( dat.survey )) == null ? '' : __t) +
-'</small>\n  ';
- if (dat.flagged){ ;
-__p += '\n    Read\n  ';
- } else { ;
-__p += '\n    Unread\n  ';
- } ;
-__p += '\n\n  ';
- if (dat.root.active){ ;
-__p += '\n    Active\n  ';
- } else { ;
-__p += '\n    Inactive\n  ';
- } ;
-__p += '\n\n  <ul>\n    ';
+'</h3>\n    ';
+ }; ;
+__p += '\n\n    <ul>\n      ';
  _.each(dat.payload, function(doc) { ;
-__p += '\n      <li>\n        <h2>' +
+__p += '\n        <li>\n          <div class="question">\n            ' +
 ((__t = ( doc.label )) == null ? '' : __t) +
-'</h2>\n        <small>' +
-((__t = ( doc.cid )) == null ? '' : __t) +
-'</small>\n        <p>' +
+'\n          </div>\n          <div class="answer">' +
 ((__t = ( doc.res_label )) == null ? '' : __t) +
-'</p>\n      </li>\n    ';
+'</div>\n        </li>\n      ';
  }); ;
-__p += '\n  </ul>\n</li>\n';
+__p += '\n    </ul>\n  </section>\n\n  <section class="">\n    <button>Analytics</button>\n    <button>More Details</button>\n  </section>\n</li>\n';
 
 }
 return __p
@@ -23163,9 +23153,13 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<li class="notification survey-ticket" data-card="parent">\n  <small><span data-livestamp="' +
+__p += '<li class="notification survey-ticket" data-card="parent">\n  <section class="title">\n    <i class="fa fa-bookmark icon"></i>\n    <p>Ticket ID: <strong>' +
+((__t = ( dat.id )) == null ? '' : __t) +
+'</strong></p>\n    <p>\n      <i class="fa fa-clock-o"></i>\n      <span data-livestamp="' +
 ((__t = ( dat.acquired )) == null ? '' : __t) +
-'">(Loading)</span></small>\n\n  <p data-survey-id="' +
+'">' +
+((__t = ( dat.acquired )) == null ? '' : __t) +
+'</span>\n    </p>\n    <button><i class="fa fa-times"></i></button>\n  </section>\n\n  <p data-survey-id="' +
 ((__t = ( dat.root_survey.id )) == null ? '' : __t) +
 '">Main Survey: ' +
 ((__t = ( dat.root_survey.name )) == null ? '' : __t) +
