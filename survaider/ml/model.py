d9e100998e8f0f2424d35c02036d58963308fcb1
Madhulika Mukherjee <madhulikamukherjee.15@gmail.com>
2016-03-22 15:35:46 +0530

    Some fixes in the ml API

--- survaider/ml/model.py
+++ survaider/ml/model.py
@@ -3,15 +3,18 @@ from bson.objectid import ObjectId
 from bson.json_util import dumps
 from survaider.survey.model import SurveyUnit
 # helper
+
+from survaider.minions.helpers import HashId
+
 def d(data):return json.loads(dumps(data))
 
 class Data(object):
 	"""docstring for Data"""
 	def __init__(self,survey_id):
-		self.sid= survey_id
+		self.sid= HashId.decode(survey_id)
 	def ext(self):
 		dat = SurveyUnit.objects(referenced = self.sid)
-		return dat
+		return d(dat)
 
 
 
