4dfb1d871c0bdaf80c58f7ddd6e73c2c13a39827
Madhulika Mukherjee <madhulikamukherjee.15@gmail.com>
2016-06-16 16:44:49 +0530

    Fixed the share link

--- frontend/SurvaiderDashboard/js/main.js
+++ frontend/SurvaiderDashboard/js/main.js
@@ -77,7 +77,8 @@
       });
  
       function DialogController($scope, $mdDialog, $location) {
-        $scope.surveyUrl = $location.absUrl();
+        // $scope.surveyUrl = $location.absUrl();
+        $scope.surveyUrl = 'http://survaider.com/survey/s:'+ uri_dat.s_id +'/simple';
         $scope.surveyName = application.companyName + ' - ' + application.unitName;
         $scope.hide = function() {
           $mdDialog.hide();
