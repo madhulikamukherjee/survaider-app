4a8cc9e2bbf332f00a327553ca00ea46f41dcef3
Madhulika Mukherjee <madhulikamukherjee.15@gmail.com>
2016-06-27 18:36:03 +0530

    Replaced bubble graph. --initial commit

---
+++ frontend/SurvaiderDashboard/js/app/models/hotelRating.js
@@ -0,0 +1,20 @@
+(function(window){
+  function hotelRating(id, name, data, labels, series){
+    this.id = id;
+    this.name = name;
+    this.data = data;
+    this.labels = labels;
+    this.series = series;
+
+    // Defaults for graph options
+    this.graphOptions = {
+        barShowStroke : true,
+        showScale: true,
+        barDatasetSpacing : 10
+    },
+    this.colors = ['#B8E986', '#92C4FF', '#B86DF9', '#F4596C', '#F7CC85'];
+  }
+
+  window.hotelRating = hotelRating;
+
+})(window);
